0eb2d65a39a0854c505d5a174f2fad57b0e5f498
yi571 <yi571@outlook.com>
2023-10-02 15:27:33 +0800

    feat: init add record time.

--- src/init.rs
+++ src/init.rs
@@ -2,6 +2,10 @@ use std::{env, fs, path::PathBuf, time::SystemTime};
 
 use crate::hashing::{self, save_csv, CsvData};
 
+use redb::{Database, Error, ReadableTable, TableDefinition};
+
+const TABLE: TableDefinition<&str, u64> = TableDefinition::new("last_modified");
+
 pub fn init(path: Option<PathBuf>) {
     let path: PathBuf = match path {
         Some(p) => p,
@@ -19,15 +23,27 @@ fn check_init_folder_exist(path: &PathBuf) {
         fs::create_dir(&init_path).unwrap();
         println!("folder created");
 
-
         let now = SystemTime::now();
-        println!("Current date and time: {:?}", now);
+        // println!("Current date and time: {:?}", now);
         match now.duration_since(SystemTime::UNIX_EPOCH) {
-            Ok(n) => init_path.push(format!("{}_init", n.as_secs())),
+            Ok(n) => {
+                let mut db_path: PathBuf = init_path.clone();
+                db_path.push("record.redb");
+                let db = Database::create(db_path).unwrap();
+                let write_txn = db.begin_write().unwrap();
+                {
+                    let mut table = write_txn.open_table(TABLE).unwrap();
+                    table.insert("last_modified", n.as_secs()).unwrap();
+                }
+                write_txn.commit().unwrap();
+
+                init_path.push(format!("{}_init", n.as_secs()))
+            }
             Err(_) => panic!("SystemTime before UNIX EPOCH!"),
         }
-        
-        let mut data: Vec<CsvData> = Vec::new();
+
+        let data: Vec<CsvData> = Vec::new();
         save_csv(init_path, data);
+        println!("init completed successfully");
     }
 }
